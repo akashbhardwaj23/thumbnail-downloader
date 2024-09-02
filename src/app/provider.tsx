ab76c00568556b342f26ddc98e19d60e26b0d392
akash23-ai <akashbhardwaj2024@gmail.com>
2024-09-02 12:13:59 +0530

    Added solana

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,18 +1,14 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
-import React, { FC, useMemo } from "react";
+import { useMemo } from "react";
 import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
 import {
   WalletModalProvider,
-  WalletDisconnectButton,
-  WalletMultiButton,
 } from "@solana/wallet-adapter-react-ui";
-import { clusterApiUrl } from "@solana/web3.js";
 import "@solana/wallet-adapter-react-ui/styles.css";
 import { CONNECTION_URL } from "./config";
 
